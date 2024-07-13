5e72ea3a67c77f2388c71720ea210a8788b41772
Akash Meshram <6388495akash@gmail.com>
2024-07-13 17:26:03 +0530

    Fix socket server

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
-from app.api import coinswitch_api_validator
+from app.api import coinswitch_api_validator, coinswitch_api_get_pairs
+from app.socket import connect_socketio
 
 def main():
     # initialize the .env file
     load_dotenv()
     # call the coinswitch api validator
     coinswitch_api_validator()
+
+    connect_socketio()
+    # coinswitch_api_get_pairs()
 
 
 if __name__ == '__main__':
